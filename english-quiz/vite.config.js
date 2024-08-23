272c37f35c361692cc3b5e27c9a18f5c9833115c
RayaElhss <webdevray07@gmail.com>
2024-08-24 00:23:58 +0300

    Update Vite config for GitHub Pages deployment

--- english-quiz/vite.config.js
+++ english-quiz/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/English-Quiz/',
+  build: {
+    outDir: 'docs',  // Outputs the build files into the 'docs' directory
+  },
 })
